9e75462407ff0cb0ae50a97c8918bfe42279d7df
juandavid <juandavidalvarez52@gmail.com>
2021-11-24 22:11:18 -0500

    deploy

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const routerApi = require('./routes/index');
 const {logErrors, errorHandler, boomError} = require('./middlewares/error.handler')
 const app = express();
-const port = 3006;
+const port = process.env.PORT || 3006;
 
 //Middleware
 app.use(express.json());
